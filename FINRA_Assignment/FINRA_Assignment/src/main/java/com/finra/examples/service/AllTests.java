8bd539224bd40d94ba241ce41c959d6f044c1bec
Yibing Li <yibingli63@gmail.com>
2018-03-04 16:24:43 -0500

    Add new testSuite file

---
+++ FINRA_Assignment/FINRA_Assignment/src/main/java/com/finra/examples/service/AllTests.java
@@ -0,0 +1,11 @@
+package com.finra.examples.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MainServiceTest.class, PhoneNumberTest.class })
+public class AllTests {
+
+}
